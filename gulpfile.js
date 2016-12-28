5d50f618143250a4822b1d10b14cb9478fddaeae
Nicolas <nicolas.jreichert@gmail.com>
2016-12-28 14:50:23 +0100

    Remove scss files and tasks/dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,8 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
-const sass = require('gulp-sass');
 const webpack = require('gulp-webpack');
 const browserSync = require('browser-sync');
 const del = require('del');
-const cleanCSS = require('gulp-clean-css');
 const historyApiFallback = require('connect-history-api-fallback');
 
 gulp.task('default', ['serve']);
@@ -13,10 +11,8 @@ gulp.task('build', [
   'clean',
   'images',
   'data',
-  'vendors',
   'template',
   'js',
-  'css',
 ]);
 
 gulp.task('set-dev-node-env', () => {
@@ -38,35 +34,16 @@ gulp.task('serve', ['build'], () => {
   });
 
   gulp.watch('./src/**/*.js*', ['js']);
-  gulp.watch('./src/scss/**/*.scss', ['css']);
   gulp.watch('./src/**/*.html', ['template']);
   gulp.watch('./src/data/*', ['data']);
 
   gulp.watch('dist/public/*.html').on('change', browserSync.reload);
   gulp.watch('dist/public/js/*.js').on('change', browserSync.reload);
-  gulp.watch('dist/public/stylesheet/*.css').on('change', browserSync.reload);
   gulp.watch('dist/data/**').on('change', browserSync.reload);
 });
 
-gulp.task('vendors', () => gulp.src('./src/vendors/**/*')
-  .pipe(gulp.dest('./dist/public/vendors'))
-);
-
 gulp.task('images', () => gulp.src('./src/assets/**/*')
   .pipe(gulp.dest('./dist/public/assets'))
-);
-
-gulp.task('css', ['minify-css'], () => del.sync(['./temp-css/**']));
-
-gulp.task('minify-css', ['stylesheets'], () => gulp.src('./temp-css/*.css')
-  .pipe(cleanCSS({ debug: true }))
-  .pipe(gulp.dest('./dist/public/stylesheet'))
-);
-
-gulp.task('stylesheets', () => gulp.src('./src/scss/**/*.scss')
-  .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('./temp-css'))
-  .pipe(browserSync.stream())
 );
 
 gulp.task('template', () => gulp.src('./src/**/*.html')
@@ -95,5 +72,4 @@ gulp.task('eslint', () => gulp.src(['./src/**/*.js', './src/**/*.jsx'])
 gulp.task('watch', () => {
   gulp.watch('./src/**/**/*.html', ['template']);
   gulp.watch('./src/**/**/*.js*', ['js', 'eslint']);
-  gulp.watch('./src/scss/**/*.scss', ['stylesheets']);
 });
