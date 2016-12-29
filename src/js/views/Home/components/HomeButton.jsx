677cea0d10d1c899ba15296ebeee7464cb764dc2
Nicolas <nicolas.jreichert@gmail.com>
2016-12-29 14:06:37 +0100

    Create HomeButton component's content

--- src/js/views/Home/components/HomeButton.jsx
+++ src/js/views/Home/components/HomeButton.jsx
@@ -1,15 +1,53 @@
 import React, { PropTypes } from 'react';
+import radium from 'radium';
+import { images } from '../../../constants';
+
+const styles = {
+  button: {
+    backgroundSize: '100% 100%',
+    width: 140,
+    height: 50,
+  },
+  1: {
+    backgroundImage: `url(${images.homeButton1})`,
+    ':hover': {
+      backgroundImage: `url(${images.homeButton1Hover})`,
+    },
+  },
+  2: {
+    backgroundImage: `url(${images.homeButton2})`,
+    ':hover': {
+      backgroundImage: `url(${images.homeButton2Hover})`,
+    },
+  },
+  3: {
+    backgroundImage: `url(${images.homeButton3})`,
+    ':hover': {
+      backgroundImage: `url(${images.homeButton3Hover})`,
+    },
+  },
+};
 
 class HomeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hover: false };
+  }
+
   render() {
+    const { index, textFront, textBack } = this.props;
     return (
-      <div>Render</div>
+      <button style={[styles.button, styles[index]]}>
+        {this.state.hover ? textBack : textFront}
+      </button>
     );
   }
 }
 
 HomeButton.propTypes = {
-
+  index: PropTypes.number.isRequired,
+  textFront: PropTypes.string.isRequired,
+  textBack: PropTypes.string.isRequired,
 };
 
-export default HomeButton;
+export default radium(HomeButton);
